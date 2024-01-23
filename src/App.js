0713fcb0a201b1ed4c5164a106512be87c6978de
Nikonova Larisa <97353756+Larrrisa@users.noreply.github.com>
2024-01-24 00:27:30 +0400

    small changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
 import Arrow from "./images/Arrow_Right.svg";
 import Logo from "./images/Cover.png";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  async function getData() {
+    try {
+      const link = "http://dushnila.gooddelo.com/data";
+      const result = await fetch(link);
+      const data = await result.json();
+      setData(data);
+      console.log(data);
+    } catch {
+      console.log("ERROR");
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(getData, 60000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div className="container">
       <h1 className="heading">Душнила</h1>
@@ -12,7 +38,7 @@ function App() {
         </div>
         <div className="sidebar-days">
           <p className="sidebar-days__header">Дней без душноты 0</p>
-          <div class="btn">
+          <div className="btn">
             <button className="sidebar-days__btn">История</button>
             <img className="arrow" src={Arrow} />
           </div>
@@ -20,13 +46,16 @@ function App() {
         <div className="sidebar-logo">
           <img className="logo" src={Logo} />
         </div>
-        <div className="sidebar-info">
+
+        <div className="sidebar-info" key="1">
           <div className="sidebar-info__temp">
-            <p className="sidebar-info__data">24.3 &#176;C</p>
+            <p className="sidebar-info__data">
+              {Number(data.temp).toFixed(1)} &#176;C
+            </p>
             <p className="sidebar-info__name">Температура</p>
           </div>
           <div className="sidebar-info__co2">
-            <p className="sidebar-info__data">543 ppm</p>
+            <p className="sidebar-info__data">{Math.floor(data.co2)} ppm</p>
             <p className="sidebar-info__name">CO2</p>
           </div>
         </div>
